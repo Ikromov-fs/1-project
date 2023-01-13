7e256f4676d31467d3430efafce918790d5b249e
Ikromov-fs <nodirikromov10@gmail.com>
2023-01-14 01:44:04 +0300

    mustaqil

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let __line2 = document.querySelector("#line2");
 let __line3 = document.querySelector("#line3");
 let __line4 = document.querySelector("#line4");
 
-
 __line1.addEventListener('click', () => {
     __slider.style.transform = "translateX(0)";
     __active.style.top = "0";
-    // __container.style.background = "#cee646"
+    __container.style.background = "linear-gradient(45deg, #f1f1f1, #010101)"
+    // background: linear-gradient
 });
 __line2.addEventListener('click', () => {
     __slider.style.transform = "translateX(-25%)";
@@ -25,33 +25,7 @@ __line4.addEventListener('click', () => {
     __active.style.top = "240px"
 });
 
-// let slider = document.querySelector("#slider");
-// let active = document.querySelector("#active");
-// let line1 = document.querySelector("#line1");
-// let line2 = document.querySelector("#line2");
-// let line3 = document.querySelector("#line3");
-// let line4 = document.querySelector("#line4");
-// let container = document.querySelector(".container");
 
-// line1.addEventListener('click', () => {
-//     slider.style.transform = "translateX(0)";
-//     active.style.top = 0;
-// });
-
-// line2.addEventListener('click', () => {
-//     slider.style.transform = "translateX(-25%)";
-//     active.style.top = "80px";
-// });
-
-// line3.addEventListener('click', () => {
-//     slider.style.transform = "translateX(-50%)";
-//     active.style.top = "160px";
-// });
-
-// line4.addEventListener('click', () => {
-//     slider.style.transform = "translateX(-75%)";
-//     active.style.top = "240px";
-// });
 
 
 
